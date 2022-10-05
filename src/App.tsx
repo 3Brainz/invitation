3415c76b0964c152c4f5c36ae9def3f2fbd65db7
Roberto Punzo <45487824+3Brainz@users.noreply.github.com>
2022-10-05 20:47:27 +0200

    finito

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import Dance from "./assets/Dance.svg";
+import InvitoScema from "./assets/invito.png";
+import guagliu from "./assets/guagliu.gif";
+
 import "./App.css";
 
 function App() {
@@ -9,26 +13,54 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={Dance} className="logo react" alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div>
-        <button onClick={() => setUser(1)}>
-          Sono Angelica Elizabeth Salvadore
-        </button>
-        <br />
-        <br />
-        <button onClick={() => setUser(2)}>Sono un altra persona</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <>{user === 1 ? <>caio</> : <>sempr</>}</>
+      <h1>INVITO UFFICIALE AL WALZER</h1>
+      <h1>15-16/10/2022</h1>
+      <>
+        {user === 0 ? (
+          <div>
+            <button onClick={() => setUser(1)}>
+              Sono Angelica Elizabeth Salvadore
+            </button>
+            <br />
+            <br />
+            <button onClick={() => setUser(2)}>Sono un altra persona</button>
+          </div>
+        ) : (
+          <></>
+        )}
+      </>
+      <>
+        {user === 1 ? (
+          <>
+            <img src={InvitoScema} className="invitoScema" alt="invitoScema" />
+            <h3>Egregia Angelica Elizabeth Salvadore</h3>
+            <h3>nonostante la sua vrenzolamma e scimitá</h3>
+            <h3>
+              é invitata allo speciale walzer che si terrá nella reggia di sua
+              maestá Roberto
+            </h3>
+            <h2>Per Elizabeth la regina della scimitá</h2>
+          </>
+        ) : (
+          <></>
+        )}
+      </>
+      <>
+        {user === 2 ? (
+          <>
+            <img src={guagliu} className="guagliu" alt="guagliu" />
+            <h3>WE FRATM</h3>
+            <h3>TI ASPETTO AL MIO PARTY</h3>
+            <h3>AMMA FA O BURDELL, STATT ACCORT CA C STA COCC SCEM</h3>
+            <h3>PUORT TUTT É CUMPAGNJ TUOIJ</h3>
+            <h2>FRAAAAAATM</h2>
+          </>
+        ) : (
+          <></>
+        )}
+      </>
     </div>
   );
 }
